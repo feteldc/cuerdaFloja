455b3a3d56cc5a84f6b726654745870099d09ffe
Damian <damian.15carrillod@gmail.com>
2022-05-17 11:58:53 -0400

    actualizado

--- controlador/controlador.py
+++ controlador/controlador.py
@@ -1,6 +1,8 @@
 import wx
 import wx.grid as gridlib
 from wx.grid import GridCellAutoWrapStringRenderer
+from hashlib import new
+
 
 class Problem(wx.Frame):
     def __init__(self):
@@ -18,11 +20,28 @@ class Problem(wx.Frame):
         self.btn1 =wx.Button(self.box1, label= "facil", size=(130,30),pos=(600,32))
                              
    
-        self.btn2= wx.Button(self.box1, label="iniciar partida",size=(150,50),pos=(300,500))
+        self.btn2= wx.Button(self.box1, label="iniciar partida",size=(150,50),pos=(300,480))
         self.lbll= wx.StaticText(self.box1, -1,"Escoger imagen",pos =(20,15))
         self.btn3=wx.Button(self.box1, label="intermedio",size=(130,30),pos=(600,65))
         self.btn4=wx.Button(self.box1, label="Dificil", size= (130,30), pos=(600,98))
 
-        self.grid = gridlib.Grid(self.panel,-1,size=(500,35),pos=(20,60))
+        self.grid = gridlib.Grid(self.panel,-1,size=(500,32),pos=(20,60))
         self.grid.SetDefaultRenderer(GridCellAutoWrapStringRenderer())
-        self.grid.CreateGrid(2, 2)
+        self.grid.CreateGrid(0, 2)
+        
+        self.archivo = wx.Menu()
+# Agregamos el sub-menú Guardar
+        self.guardar = wx.MenuItem(self.archivo,-1,"Guardar")
+        self.guardar.SetBitmap(wx.Bitmap( u"img/ic_save.png", wx.BITMAP_TYPE_ANY ))
+        self.archivo.AppendItem(self.guardar)
+# Agregamos el sub-menú Abrir
+        self.abrir = wx.MenuItem(self.archivo,-1,"Abrir")
+        self.abrir.SetBitmap(wx.Bitmap( u"img/ic_open.png", wx.BITMAP_TYPE_ANY ))
+        self.archivo.AppendItem(self.abrir)
+    
+    # Creamos la barra de menú principal y la configuramos
+        self.mb = wx.MenuBar()
+        self.mb.Append(self.archivo, "Archivo")
+        self.SetMenuBar(self.mb)
+        
+    
--- vista/Main.py
+++ vista/Main.py
@@ -1,5 +1,6 @@
 import wx
 from controlador.controlador import Problem
+ 
         
 if __name__ == "__main__":
     app = wx.App()
